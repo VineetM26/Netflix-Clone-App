65410721f6ff6059f2e12eee40b1f9dfc3ae7610
Maddheshiya <vmaddheshiya@tnsi.com>
2024-03-19 23:44:59 +0530

    updated card

--- src/components/card.js
+++ src/components/card.js
@@ -68,9 +68,26 @@ const Card = () => {
 
 const CardContainer = styled.div`
 
-/* max-width: 230px; */
-background-color: red;
+    max-width: 230px;
+    width: 230px;
+    height: 100%;
+    cursor: pointer;
+    position: relative;
+    background-color: red;
+    img{
+        border-radius: 0.2rem;
+        width: 100%;
+        height: 100%;
+        z-index: 10;
+    }
+    .hover{
+        z-index: 99;
+        height: max-content;
+        width: 20rem;
+        position: absolute;
+        top: -18
+    }
 
-`
+`;
 
 export default Card
