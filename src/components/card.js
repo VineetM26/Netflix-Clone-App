54ac663a91bddd2cc3c56612920f76d0f9e53af9
Maddheshiya <vmaddheshiya@tnsi.com>
2024-03-20 19:49:09 +0530

    setting up API

--- src/components/card.js
+++ src/components/card.js
@@ -33,7 +33,7 @@ const Card = () => {
                         autoPlay loop controls
                         />
                     </div>
-                    <div className='info -container'>
+                    <div className='info-container'>
                         <h3 className='movieName' onClick={()=>navigate('/player')}>Red Notice</h3>
                         <div className='icons'>
                             <div className='controls'>
@@ -50,7 +50,7 @@ const Card = () => {
                                 <BiChevronDown title='More info'/>
                             </div>
                         </div>
-                        <div className='genre'>
+                        <div className='genres'>
                             <ul>
                                 <li>Action</li>
                                 <li>Action</li>
@@ -67,7 +67,7 @@ const Card = () => {
 }
 
 const CardContainer = styled.div`
-
+    margin-top: 1rem;
     max-width: 230px;
     width: 230px;
     height: 100%;
@@ -85,7 +85,77 @@ const CardContainer = styled.div`
         height: max-content;
         width: 20rem;
         position: absolute;
-        top: -18
+        top: -18vh;
+        left: 0;
+        border-radius: 0.2rem;
+        border: 0.1rem solid gray;
+        background-color: #181818;
+        transition: 0.3s ease-out;
+        .image-video-wrapper{
+            position: relative;
+            height: 140px;
+            img{
+                width: 100%;
+                height: 150px;
+                object-fit: cover;
+                border-radius: 0.3rem;
+                top: 0;
+                z-index: 4;
+                position: absolute;
+            }
+            video{
+                width: 100%;
+                height: 150px;
+                object-fit: cover;
+                border-radius: 0.3rem;
+                top: 0;
+                z-index: 4;
+                position: absolute;
+            }
+        }
+        .info-container{
+            display: flex;
+            flex-direction: column;
+            padding: 1rem;
+            gap: 0.5rem;
+            .movieName{
+                color: white;
+            }
+        }
+        .icons{
+            display: flex;
+            justify-content: space-between;
+            .controls{
+                display: flex;
+                gap: 0.5rem;
+            }
+        svg{
+            color: white;
+            border: 0.1rem solid white;
+            border-radius: 50%;
+            font-size: 1.5rem;
+            cursor: pointer;
+            transition: 0.3s ease-in-out;
+            &:hover{
+                color: #b8b8b8;
+            }
+        }
+        }
+        .genres{
+            display: flex;
+            color: white;
+            ul{
+                display: flex;
+                gap: 1rem;
+                li{
+
+                    padding-right: 0.7rem;
+                    &:first-of-type{
+                        list-style-type: none;
+                    }
+                }
+            }
+        }
     }
 
 `;
---
+++ src/utils/constant.js
@@ -0,0 +1,2 @@
+export const MY_API_KEY = "0c08858413a4e64bf3a53a745e0fef80"
+export const TMDB_BASE_URL = "https://api.themoviedb.org/3"
